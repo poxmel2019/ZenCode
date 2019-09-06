9c33fe208847fda110cb9f4176c61c1b7fc8991d
Madiyar <nu_ofsn11@mail.ru>
2019-09-06 12:09:54 +0600

    plus txt

--- pl.py
+++ pl.py
@@ -1,6 +1,7 @@
 import tkinter
 import openpyxl as op
 import numpy
+import io
 
 def f():
     "This is excel"
@@ -29,11 +30,35 @@ def f():
             score.append(y.value)
     average_age = av_ag(age)
     average_score = av_ag(score)
+    mas1 = ["Items","Average age","Max score","Min score:","Average score"]
+    mas2 = []
+    mas2.append(len(ids))
+    mas2.append(average_age)
+    mas2.append(max(score))
+    mas2.append(min(score))
+    mas2.append(average_score)
+    
     print("Items:",len(ids))
     print("Average age:",average_age)
     print("Max score:",max(score))
     print("Min score:",min(score))
     print("Average score:",average_score)
+    
+
+    with open("res.txt",'w') as file:
+        i = 0
+        for x in mas1:
+            file.write(mas1[i])
+            file.write(' - ')
+            file.write(str(mas2[i]))
+            file.write('\n')
+            i += 1
+            
+        #i = 0
+        #for x in file:
+        #    x.writelines(mas1[i])
+        #    i += 1
+    
 
 def av_ag(age):
     a = 0
